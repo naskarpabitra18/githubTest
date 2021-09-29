2d129a6ece1aff7a534e93e99b6f5172fa6c1372
Pabitra Naskar <91193616+naskarpabitra18@users.noreply.github.com>
2021-09-30 05:01:25 +0530

    Add files via upload

---
+++ random_number_guess_game.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int num, n, nguesses=1;
+    srand(time(0));
+    num=rand()%100+1;
+    // printf("the value of the number is %d\n", num);
+    do{
+        printf("enter a number between 1-100\n");
+        scanf("%d", &n);
+        if(num>n){
+            printf("higher number plz!\n");
+            }
+
+            else if(num<n){
+                printf("lower rnumber plz!\n");
+            }
+            else{
+                printf("you guessed it right in %d attempts\n", nguesses);
+                }
+                nguesses++;
+    }
+                while(n!=num);
+             return 0;
+}
